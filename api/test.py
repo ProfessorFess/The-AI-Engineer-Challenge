0e79fb3a3822ed599f13683a057882fdc3ef1b62
Professor Myhre <professormyhre@gmail.com>
2025-09-23 21:10:19 -0500

    Restore working app.py configuration

--- api/test.py
+++
@@ -1,6 +0,0 @@
-def handler(event, context):
-    return {
-        'statusCode': 200,
-        'headers': {'Content-Type': 'application/json'},
-        'body': '{"status": "minimal_python_working", "message": "Basic Python function works"}'
-    }
